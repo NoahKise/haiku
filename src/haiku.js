23da90ce2404a3486035f793baca90284f723c0d
noahkise <noah@kisefamily.com>
2023-11-08 11:06:27 -0800

    pass syllableChecker test 4
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

--- src/haiku.js
+++ src/haiku.js
@@ -8,6 +8,13 @@ export default class Haiku {
         const digraphBlendArray = ["bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"];
         let poemLetterArray = this.poem.split("");
         let outputArray = [];
+        if (poemLetterArray[poemLetterArray.length-1] === "e" && poemLetterArray[poemLetterArray.length-2] === "l" && (!vowelArray.includes(poemLetterArray[poemLetterArray.length-3]))) {
+            let firstSyllable = poemLetterArray.slice(0, -3);
+            let secondSyllable = poemLetterArray.slice(-3);
+            outputArray.push(firstSyllable.join(""));
+            outputArray.push(secondSyllable.join(""));
+            return outputArray;
+        }
         for (let i = 0; i < poemLetterArray.length; i++) {
             if (!vowelArray.includes(poemLetterArray[i])) {
                 if (!vowelArray.includes(poemLetterArray[i + 1])) {
