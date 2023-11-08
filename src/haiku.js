73d8737e7017866e2adc12abb022155b6bcfcde0
noahkise <noah@kisefamily.com>
2023-11-08 10:17:29 -0800

    pass syllableChecker test 1
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>
    Co-authored-by: Teddy Peterschmidt <t.a.peterschmidt@gmail.com>

--- src/haiku.js
+++ src/haiku.js
@@ -4,6 +4,22 @@ export default class Haiku {
     }
 
     syllableChecker() {
+        const vowelArray = ["a", "e", "i", "o", "u"];
+        let poemLetterArray = this.poem.split("");
+        let outputArray = [];
+        for (let i = 0; i < poemLetterArray.length; i++) {
+            if (!vowelArray.includes(poemLetterArray[i])) {
+                if (!vowelArray.includes(poemLetterArray[i+1])) {
+                    let firstSyllable = poemLetterArray.slice(0, i+1);
+                    let secondSyllable = poemLetterArray.slice(i+1);
+                    outputArray.push(firstSyllable.join(""));
+                    outputArray.push(secondSyllable.join(""));
+                    break;
+                }
 
+            }
+        }
+        console.log(outputArray);
+        return outputArray;
     }
 }
